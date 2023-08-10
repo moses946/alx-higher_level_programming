9a1937750f9a5b113fe47e2e3cab36ae4c8381b0
moses946 <mosesmwaiw@gmail.com>
2023-08-10 15:22:38 +0300

    Add return statement to function

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if type(number) != int:
-        return TypeError
-    print("{}".format(str(number)[-1]), end='')
+    print("{}".format(str(int(number))[-1]), end='')
     return str(number)[-1]
