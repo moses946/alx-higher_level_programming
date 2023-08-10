c0ee39783c4fb26ec6f4a8acc311c8c4429c479d
moses946 <mosesmwaiw@gmail.com>
2023-08-10 14:59:02 +0300

    Add python3 specification

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def uppercase(str):
     for i in str:
         val = ord(i) if 97 > ord(i) <= 122 else ord(i) - 32
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,2 +1,4 @@
+#!/usr/bin/python3
+
 def print_last_digit(number):
     print("{}".format(str(number)[-1]))
