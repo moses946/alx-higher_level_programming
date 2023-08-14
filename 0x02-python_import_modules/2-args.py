47404a3f14d272b81d8be79120fe2d02cd1ca7e3
moses946 <mosesmwaiw@gmail.com>
2023-08-14 17:27:39 +0300

    Initial commit

---
+++ 0x02-python_import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+	print("{} arguments.".format(0))
+elif len(argv) == 2:
+	print("{} argunment:\n{}: {}".format(len(argv) - 1, 1, argv[1]))
+else:
+	print("{} arguments:".format(len(argv) - 1))
+	for i in range(1,len(argv)):
+		print("{}: {}".format(i, argv[i]))
