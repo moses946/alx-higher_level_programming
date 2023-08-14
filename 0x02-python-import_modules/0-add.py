a69d651dfda71b490af57c1160adf67e249f850b
moses946 <mosesmwaiw@gmail.com>
2023-08-14 17:44:46 +0300

    Fix errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from add_0.py import add
+from add_0 import add
 a = 1
 b = 2
 if __name__ == "__main__":
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 result = 0
-if len(argv) > 1:
-    for i in range(1, len(argv)):
-        result += int(argv[i])
+if __name__ == "__main__":
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            result += int(argv[i])
