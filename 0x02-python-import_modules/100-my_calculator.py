4238bfbdbfd9d7dc401fcbcc792b92fb5d272350
moses946 <mosesmwaiw@gmail.com>
2023-08-15 20:17:16 +0300

    Add advanced task 6.

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import *
+operators = ['+', '-', '*', '/']
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print(len(argv))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    result = eval(str(int(argv[1]))+argv[2]+str(int(argv[3])))
+    print(f"{argv[1]} {argv[2]} {argv[3]} = {result}")
