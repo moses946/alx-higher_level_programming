ed699e904aedac2f1fdd7273ff976fd0b6f20033
moses946 <mosesmwaiw@gmail.com>
2023-08-15 19:17:08 +0300

    Fix errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[]):
     for row in matrix:
         for idx, val in enumerate(row):
             if idx != len(row) - 1:
-                print("{:d} ".format(int(val)),end='')
+                print("{:d} ".format(int(val)), end='')
             else:
                 print("{:d}".format(int(val)), end='')
         print()
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    max_val = 0
+    max_val = int('-inf')
     for i in my_list:
         max_val = int(i) if int(i) > max_val else max_val
     return max_val
