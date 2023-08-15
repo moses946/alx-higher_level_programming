e9736dbde590e27ef354f249117b5d70a2ebb642
moses946 <mosesmwaiw@gmail.com>
2023-08-15 19:48:37 +0300

    Fix errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,6 @@
 from add_0 import add
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+ans = add(int(a), int(b))
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, ans))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 sum = 0
-for i in range(1, len(argv)):
-    sum += int(argv[i])
-print("{:d}".format(sum))
+if __name__ == "__main__":
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print("{:d}".format(sum))
