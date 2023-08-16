13ed82eced6f31ebfbba1ec5482b0b5e30c1b20b
moses946 <mosesmwaiw@gmail.com>
2023-08-17 02:30:44 +0300

    Fix errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    r = roman_string
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    r = roman_string.upper()
     table = {
         'I': 1,
         'V': 5,
@@ -10,10 +12,13 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
+    k = list(table.keys())
     i = 0
     result = 0
     while i < len(r):
-        if list(table.keys()).index(r[i]) < list(table.keys()).index(r[i + 1]):
+        if r[i] not in k:
+            return 0
+        if i < len(r) - 1 and k.index(r[i]) < k.index(r[i + 1]):
             temp = table[r[i + 1]] - table[r[i]]
             result += temp
             i += 2
