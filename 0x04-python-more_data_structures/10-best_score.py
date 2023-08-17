afa891d4bdea8a128649957de2d71bc82c8e0d4b
moses946 <mosesmwaiw@gmail.com>
2023-08-17 03:50:15 +0300

    Fix error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,6 +8,6 @@ def best_score(a_dictionary):
         if val is not None and val > max_val:
             max_val = val
             max_key = key
-        if max_val == float('-inf'):
-            return None
+    if max_val == float('-inf'):
+        return None
     return max_key
