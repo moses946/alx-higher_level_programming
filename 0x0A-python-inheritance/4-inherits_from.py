c582069a97effc4e3c4f541e4819a1063d96ed27
moses946 <mosesmwaiw@gmail.com>
2023-09-12 13:36:14 +0300

    Rewrite code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,6 @@
 def inherits_from(obj, a_class):
     """ Returns True if the object is an instance of a class
     """
-    return issubclass(obj, a_class)
+    if isinstance(obj, a_class) and not issubclass(a_class, obj.__class__):
+        return True
+    return False
