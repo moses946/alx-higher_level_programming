2da00e499ac9933329797bcb6f007e1ec6e108e8
moses946 <mosesmwaiw@gmail.com>
2023-09-12 14:11:20 +0300

    Add blankline after import statement

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,6 +6,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Square class inheriting form Rectangle Class"""
     def __init__(self, size):
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,6 +5,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class inheriting from BaseGeometry class"""
     def __init__(self, width, height):
@@ -15,6 +16,6 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
-    
+
     def area(self):
         return self.__width * self.__height
