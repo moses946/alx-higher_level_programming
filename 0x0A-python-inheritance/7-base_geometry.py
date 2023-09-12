31c32d1ec911926d2e99cdb939e2a9cd97967d6f
moses946 <mosesmwaiw@gmail.com>
2023-09-12 15:26:41 +0300

    Fix validation for object type

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,7 +17,7 @@ class BaseGeometry(object):
             name :(string) Name
             value :(int) Integer
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
