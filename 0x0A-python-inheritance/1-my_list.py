17b9e44e3a7ca67df5a9efb86da86220bb83f289
moses946 <mosesmwaiw@gmail.com>
2023-09-12 14:48:29 +0300

    Add test file for 1-my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,23 +6,11 @@
 class MyList(list):
     """ Class MyList that inherits from list
     """
-
-
     def print_sorted(self):
-        def partition(lst):
-            mid = max(lst) / min(lst)
-            for val in  lst:
-                lo = []
-                hi = []
-                if val < mid:
-                    lo.append(val)
-                else:
-                    hi.append(val)
+        """Prints a sorted list"""
+        print(sorted(self))
 
-        def quicksort(lst):
-            if len(lst) <= 1:
-                return lst
-            lo, mid, hi = partition(lst)
-            return quicksort(lo) + [mid] +quicksort(hi)
 
-        print(quicksort(self))
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/1-my_list.txt")
