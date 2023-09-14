4f550184b478978a28d886a4565689dd8f860fbe
moses946 <mosesmwaiw@gmail.com>
2023-09-14 16:49:56 +0300

    Initial commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" append_after module """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ Appends a line of text after each line contanining a specific string
+    Args:
+        filename(str): name of file to read and append
+        search_string(str): search string
+        new_string(str): string to append
+    """
+    if search_string == "":
+        return
+    with open(filename, 'r', encoding="utf-8") as fileptr:
+        content = fileptr.readlines()
+    for idx, line in enumerate(content):
+        if search_string in line:
+            content.insert(idx + 1, new_string)
+    with open(filename, 'w') as fileptr:
+        fileptr.writelines(content)
