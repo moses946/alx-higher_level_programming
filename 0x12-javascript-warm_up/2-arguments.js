430d6fa41c42e2c9a15aae22d6bd7e4cfad8ae2a
moses946 <mosesmwaiw@gmail.com>
2024-01-09 03:08:54 +0300

    Fix errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length < 3) {
+const process = require('node:process');
+if (process.argv.length < 3) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
