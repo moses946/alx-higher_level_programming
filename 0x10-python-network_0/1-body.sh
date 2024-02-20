e5238fed87863af0737a6b3946c69a6cd2e072c9
moses946 <mosesmwaiw@gmail.com>
2024-02-21 00:49:44 +0300

    Rewrite file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that displays the body of 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+curl -sL -w "%{http_code}\\n" "$1" -o /dev/null | grep -q "200" && curl -sL "$1"
