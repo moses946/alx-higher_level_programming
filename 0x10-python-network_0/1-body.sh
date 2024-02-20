e9c43c3909b30c5bb9404324a0e7e28c0569a323
moses946 <mosesmwaiw@gmail.com>
2024-02-21 00:31:50 +0300

    Add newline at end of file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that displays the body of 200 status code response
-curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi.
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
