4d8ba4e77cc441cb838b298b9ec7fe6364cecc41
moses946 <mosesmwaiw@gmail.com>
2024-03-03 18:10:52 +0300

    Add sql query

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 CREATE USER IF NOT EXISTS 'user_0d_1@localhost' 
 	IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL 
+GRANT ALL PRIVILEGES
 	ON *.* 
-	TO 'user_0d_1@localhost';
+	TO 'user_0d_1@localhost' WITH GRANT OPTION;
