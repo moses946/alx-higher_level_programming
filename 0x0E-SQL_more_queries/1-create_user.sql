379d98b68b863e218cd3988d61527affab1518e2
moses946 <mosesmwaiw@gmail.com>
2024-03-03 18:12:22 +0300

    Add sql query

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create a new MYSQL user
-CREATE USER IF NOT EXISTS 'user_0d_1@localhost' 
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
 	IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES
